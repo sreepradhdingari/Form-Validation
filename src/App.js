dea3286e5cac56040349b41d06ddd9c5c46313f6
sreepradhdingari <dingarisreepradh9@gmail.com>
2023-04-20 12:58:57 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,18 @@
-import React, {useState} from 'react'
+import React, {useEffect,useState} from 'react'
+import axios from 'axios'
 
-export default function App(){
-  const [data,setData] = useState({
-    username:"",
-    email:"",
-    password:"",
-    confirmpassword:""
-  })
-  const {username,email,password,confirmPassword} = data
-  const clickHandler = event => {
-    setData({...data,[event.target.name]:event.target.value})
-  }
-  const submitHandler = event => {
-    event.preventDefault()
-    if (password !== confirmPassword){
-      alert("Passwords are not matching")
-    }else if(username.length < 5){
-      alert("Username must be atleast 5 charecters")
-    }else{
-      console.log(data)
-    }
-  }
+const App = () => {
+  const [data,setData] = useState([])
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/todos").then(
+      response => setData(response.data)
+    )
+  },[])
   return(
     <div>
-      <form onSubmit={submitHandler}>
-        <input type = "text" name = "username" value = {username} placeholder='Username' onChange = {clickHandler}/><br/>
-        <input type = "email" name = "email" value = {email} placeholder='Email' onChange = {clickHandler}/><br/>
-        <input type = "password" name = "password" value = {password} placeholder='Password' onChange = {clickHandler}/><br/>
-        <input type = "password" name = "confirmPassword" value = {confirmPassword} placeholder='Confirm Password' onChange = {clickHandler}/><br/>
-        <input type = "submit" name = "submit"/>
-      </form>
+        {data.map(item => <li key={item.id}>{item.title}</li>)}
     </div>
   )
 }
+
+export default App
