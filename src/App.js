5a78447d459253aec910ad0035dbddf2bb0488ce
sreepradhdingari <dingarisreepradh9@gmail.com>
2023-04-20 09:28:50 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App(){
+  const [data,setData] = useState({
+    username:"",
+    email:"",
+    password:"",
+    confirmpassword:""
+  })
+  const {username,email,password,confirmPassword} = data
+  const clickHandler = event => {
+    setData({...data,[event.target.name]:event.target.value})
+  }
+  const submitHandler = event => {
+    event.preventDefault()
+    if (password !== confirmPassword){
+      alert("Passwords are not matching")
+    }else if(username.length < 5){
+      alert("Username must be atleast 5 charecters")
+    }else{
+      console.log(data)
+    }
+  }
+  return(
+    <div>
+      <form onSubmit={submitHandler}>
+        <input type = "text" name = "username" value = {username} placeholder='Username' onChange = {clickHandler}/><br/>
+        <input type = "email" name = "email" value = {email} placeholder='Email' onChange = {clickHandler}/><br/>
+        <input type = "password" name = "password" value = {password} placeholder='Password' onChange = {clickHandler}/><br/>
+        <input type = "password" name = "confirmPassword" value = {confirmPassword} placeholder='Confirm Password' onChange = {clickHandler}/><br/>
+        <input type = "submit" name = "submit"/>
+      </form>
     </div>
-  );
+  )
 }
-
-export default App;
